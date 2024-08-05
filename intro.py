89f7a33376d22d8832f7abf99d5135d647684adc
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-08-05 15:56:44 +0530

    datatypes added

---
+++ intro.py
@@ -0,0 +1,29 @@
+# print('welcome to all')
+# print('hello')
+# roll_no=21
+# print(roll_no)
+# name="Roshan"
+# print(name)
+# weight=70.50
+# print(weight)
+integer=999
+print(type(integer))
+float=(1.0)
+print(type(float))
+a=10+5j
+print(type(a))
+name="Devu"
+print(type(name))
+a=True
+b=False
+print(type(a),type(b))
+List=['vavchi','aromal','unni','kannappi']
+print(type(list))
+tuple=('prince','vivek','abin','sai')
+print(type(tuple))
+set={1,2,3,4}
+print(type(set))
+data={'name':'chugil'}
+print(type(data))
+
+
