56ae3772e19df1642964fa8f78c9825520741dd5
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-08-06 18:05:56 +0530

    type cast added

--- intro.py
+++ intro.py
@@ -6,24 +6,42 @@
 # print(name)
 # weight=70.50
 # print(weight)
-integer=999
-print(type(integer))
-float=(1.0)
-print(type(float))
-a=10+5j
-print(type(a))
-name="Devu"
-print(type(name))
-a=True
-b=False
-print(type(a),type(b))
-List=['vavchi','aromal','unni','kannappi']
-print(type(list))
-tuple=('prince','vivek','abin','sai')
-print(type(tuple))
-set={1,2,3,4}
-print(type(set))
-data={'name':'chugil'}
-print(type(data))
+# integer=999
+# print(type(integer))
+# float=(1.0)
+# print(type(float))
+# a=10+5j
+# print(type(a))
+# name="Devu"
+# print(type(name))
+# a=True
+# b=False
+# print(type(a),type(b))
+# List=['vavchi','aromal','unni','kannappi']
+# print(type(list))
+# tuple=('prince','vivek','abin','sai')
+# print(type(tuple))
+# set={1,2,3,4}
+# print(type(set))
+# data={'name':'chugil'}
+# print(type(data))
+# Name =input("Enter your name : ")
+# Age =int(input("Enter your age : "))
+# Weight =float(input("Enter your weight : "))
+# print('Name',Name)
+# print('Age',Age)
+# print('Weight',Weight)
+# """a=21
+# print('a',type(a),a)
+# b=str(a)
+# print('b',type(b),b)
+# c=int(b)
+# print('c',type(c),c)
+# print("How\nare\nyou ")
+# """print("""i am
+# fine...
+# thanks""")
+item = input("Enter the item")
+price=int(input("Enter the price"))
 
 
