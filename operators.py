c092a9e3e86e6262dd42c3bec6eeb49402b9be7c
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-08-07 17:06:07 +0530

    operators added

---
+++ operators.py
@@ -0,0 +1,111 @@
+# Arithematic operator
+
+'''print(10+5)
+print('Roshan '+'R')
+print(5-3)
+print(4/2)
+print(7*7)
+print('yum'*3)
+print(10%5)
+print(4//2)
+print(5**4)'''
+
+# Assignment operator
+
+"""a=10
+print(a)
+a=10
+a=a+5
+print(a)
+a=10
+a+=5
+print(a)
+a=10
+a=a-5
+print(a)
+a=10
+a-=5
+print(a)
+a=10
+a=a*5
+print(a)
+a=10
+a*=5
+print(a)
+a=a/5
+print(a)
+a=10
+a=a/5
+print(a)
+a=10
+a=a%5
+print(a)
+a=10
+a%=5
+print(a)
+a=10
+a//=5
+print(a)
+a=10
+a**=2
+print(a)"""
+
+# Comparison operator
+
+'''print(10==10)
+print(10==9)
+print(10!=9)
+print(10!=10)
+print(10>9)
+print(10>11)
+print(10<9)
+print(10<11)
+print(10<=10)
+print(10<=9)
+print(10<=11)
+print(10>=10)
+print(10>=9)
+print(10>=11)'''
+
+# Logical operator
+
+'''print(10==10 and 10>=5)
+print(10==10 and 10<=5)
+print(10<=5 and 10==10)
+print(10==11 and 10!=10)
+print(10==10 or 10>=5)
+print(10==10 or 10<=5)
+print(10<=5 or 10==10)
+print(10==11 or 10!=10)
+print(not(10==11))'''
+
+# Membership operator
+
+"""print('e' in 'welcome')
+print('e' in 'sam')
+print('a' not in 'roshan')
+print('a' not in 'fit')
+a=[1,2,3]
+print(1 in a)
+a=[1,2,3]
+print(7 in a)"""
+
+# identity operator
+
+'''a=10
+print(id(a))
+a=10
+b=10
+print(a==b)
+print(a is b)
+print(a is not b)
+a=10
+b=11
+print(a==b)
+print(a is b)
+s=[10,20]
+s1=[10,20]
+print(s==s1)
+print(s is s1)
+print(id(s))
+print(id(s1))'''
