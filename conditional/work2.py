6656eee6451ec3faefb42bf30015fa42a58a7b4e
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-08-08 17:37:51 +0530

    conditional statements added

---
+++ conditional/work2.py
@@ -0,0 +1,6 @@
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+if a>b:
+    print("first number is large")
+else:
+    print("second numebr is large")
---
+++ string.py
@@ -0,0 +1,49 @@
+a="Welcome"
+'''print(a)
+print(a[0])
+print(a[-6])'''
+
+# slicing
+
+'''print(a[0:4])
+print(a[:4])
+print(a[-4:])
+print(a[3:6])
+print(a[:-4])
+print(a[::-1])
+print(a[::2])'''
+
+
+'''print(a.capitalize())
+print(a.center(30))
+print(a.count('e'))
+print(a.count('o'))
+print(a.endswith('a'))
+print(a.endswith('e'))
+print(a.find('a'))
+print(a.find('c'))
+print(a.index('e'))
+print(a.islower())
+b='456'
+print(b.isdigit())
+print(a.isdigit())'''
+
+
+'''a='      welcome to all         '
+print(a.split())
+print(a.lstrip())
+print(a.rstrip())'''
+
+# Format
+
+'''name    age     marks
+    a       3       23
+    v      4       23
+    e       7        89'''
+
+print("{:10}{:8}{:8}".format("Name","Age","Marks"))
+print("{:10}{:8}{:8}".format("Raju","19","50"))
+print("{:10}{:8}{:8}".format("Radha","19","82"))
+print("{:10}{:8}{:8}".format("Maya V","20","100"))
+
+
