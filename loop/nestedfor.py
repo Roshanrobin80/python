b307dbace66cccabfc03ef09d2dcd0e9b444a1b3
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-08-21 09:02:03 +0530

    nested lopp updted

--- loop/nestedfor.py
+++ loop/nestedfor.py
@@ -24,11 +24,52 @@ for i in range(3):
 #0 1 2
 #2 1 0
 
-for i in range(4):
+'''for i in range(4):
     for j in range(3):
         if i%2==0:
             print(j,end=' ')
         else:
             print(2-j,end=' ')
-    print()
+    print()'''
+
+#0
+#0 1
+#0 1 2
+
+'''for i in range(1,4):
+    for j in range(i):
+        print(j,end=' ')
+    print()'''
+
+#0
+#1 2 
+#3 4 5
+
+'''a=0
+for i in range(1,4):
+    for j in range(i):
+        print(a,end=' ')
+        a+=1
+    print()'''
+
+#0
+#1 1
+#2 2 2
+
+'''for i in range(1,4):
+    for j in range(i):
+        print(i-1,end=' ')
+    print()'''
+
+#0
+#1 0
+#2 1 0
+
+'''a=0
+for i in range(1,4):
+    for j in range(i):
+        print(a-j,end=' ')
+    a+=1
+    print()'''
+
         
