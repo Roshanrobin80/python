dbad4ea7ce2df8e4eaf3604d989760b8a5edb119
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-08-23 09:34:34 +0530

    list updated

---
+++ list/list.py
@@ -0,0 +1,43 @@
+'''l=[1,2,10,20,'abc',1]
+print(l[0])
+print(l[1])
+if 10 in l:
+    print('available')
+else:
+    print('not available')
+for i in l:
+    print(i)
+l[1]=7
+print(l)'''
+
+#list methods
+#adding
+'''l=[]
+l.append(10)
+l.append('abc')
+l.append([21,80,9])
+if 21 in l:
+    print('avl')
+else:
+    print('not')
+l.extend([444,555,666])
+l.insert(3,7)
+print(l)'''
+
+#delete
+
+'''l=[10,20,30]
+l.clear()
+l.pop(0)
+l.remove(30)
+print(l)'''
+
+#index,sort(ascending),reverse
+
+'''l=[7,2,10,6,9,7]
+print(l.index(10))
+print(l.count(7))
+l.sort()
+l.reverse()
+print(l)'''
+
