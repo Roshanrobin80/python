0a216a7c96285b7decb9f33a4c264682e4ce6518
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-08-24 09:21:02 +0530

    list updated

--- list/list.py
+++ list/list.py
@@ -41,3 +41,48 @@ l.sort()
 l.reverse()
 print(l)'''
 
+#sum of list
+
+'''l=[1,10,12,'abc',8.5]
+sum=0
+for i in l:
+    if type(i)==int or type (i)==float:
+        sum+=i
+    print(sum)'''
+
+#sum of odd no.s
+
+'''l=[10,1,2,3,5,8,6]
+o=0
+e=0
+for i in l:
+    if i%2!=0:
+        o+=i
+    else:
+        e+=i
+print('sum of odd no.s= ',o)
+print('sum of even no.s= ',e)'''
+
+#avoid duplication
+
+'''l=[10,1,2,3,5,8,6,1,3,8]
+for i in l:
+    if l.count(i)>=2:
+        l.remove(i)
+print(l)'''
+
+#set method
+
+'''l=[10,1,2,3,5,8,6,1,3,8]
+s=set(l)
+l=list(s)
+print(l)'''
+
+#3 method
+ 
+'''l=[10,1,2,3,5,8,6,1,3,8]
+l1=[]
+for i in l:
+    if i not in l1:
+        l1.append(i)
+print(l1)'''
