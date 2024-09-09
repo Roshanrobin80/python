aab22767cf9440ac28dfd03c911cb4c7ee034be1
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-09-09 09:10:27 +0530

    set updated

--- set/set.py
+++ set/set.py
@@ -26,7 +26,7 @@ s.clear()
 s.copy()
 print(s)'''
 
-s={1,2,3,4,5}
+'''s={1,2,3,4,5}
 #s1={1,2,3,6}
 # print(s.difference(s1))
 # print(s.union(s1))
@@ -38,9 +38,46 @@ s1={1,2,3,4,5,6}
 #print(s.issubset(s1))
 #print(s1.issuperset(s))
 #print(s.update({10,11,12}))
-#print(s.difference_update(s1))
+#print(s.difference_update(s1))'''
+
+#input names & display on set
+
+'''s=set()
+a=int(input('Enter the limit: '))
+for i in range(a):
+    b=str(input('Enter name: '))
+    s.add(b)
+print(s)'''
+
+#php=   {a,b,c}
+# java= {a,c,d,e}
+#python={a,b,d,f,g}
+
+php=set()
+a=int(input('Enter the limit: '))
+for i in range(a):
+    b=str(input('Enter name: '))
+    php.add(b)
 
 
+java=set()
+e=int(input('Enter the limit: '))
+for i in range(e):
+    c=str(input('Enter name: '))
+    java.add(c)
 
+
+python=set()
+f=int(input('Enter the limit: '))
+for i in range(f):
+    d=str(input('Enter name: '))
+    python.add(d)
+print('python =',python)
+print('php =',php)
+print('java =',java)
+inter=php.intersection(java)
+print('Common in 3 sub =',inter.intersection(python))
+diff=php.symmetric_difference(java)
+print('Only in 1 sub =',diff.symmetric_difference(python))
 
 
