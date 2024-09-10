87af025062d53d5f52c4b9cb3e0415a85dfa0227
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-09-10 09:05:07 +0530

    function started

---
+++ function/function.py
@@ -0,0 +1,31 @@
+'''def sample():
+    print('carlos')
+    print('cafu')
+    print('kaka')
+a=[1,2,3,4,5]
+print(a)
+a.append(10)
+sample()
+a=[1,2,3,4,5]
+print(a)
+a.append(10)
+sample()'''
+
+#calling global and local variable
+
+'''def sample():
+    b=10            #local variable
+    print('inside function a=',a)
+    print('inside function b=',b)
+a=20                #global variable
+sample()
+print('outside function a=',a)
+print('outside funcyion b=',b)'''
+
+'''def sample():
+    a=10
+    print('inside function a =',a)
+a=20
+sample()
+print('outside function a =',a)'''
+
