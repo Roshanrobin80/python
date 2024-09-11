f9caac7ab180026545d6fa9ae04d2d6ac01114d8
Roshanrobin80 <roshanrobin220303@gmail.com>
2024-09-11 09:05:39 +0530

    functions updatted

--- function/function.py
+++ function/function.py
@@ -29,3 +29,84 @@ a=20
 sample()
 print('outside function a =',a)'''
 
+#global keyword
+
+'''def sample():
+    global a
+    a=10
+    print('welcome',a)
+sample()
+print(a)
+'''
+#return keyword
+
+'''def sample():
+    a=10
+    b=20
+    return 'welcome',a,b
+c,a1,b1=sample()
+print(a1)
+print(b1)
+print(c)'''
+
+#arithematic operations 
+
+'''def number():
+    a=int(input('Enter a number: '))
+    b=int(input('Enter a number: '))
+    return a,b
+def add():
+    a,b=number()
+    print(a+b)
+
+while True:
+    print(''''''
+1.add
+2.sub
+3.mul
+4.div
+5.flr div
+6.exit
+      '''''')
+    ch=int(input('Enter your choice: '))
+    if ch==1:
+        add()
+
+    elif ch==2:
+        a,b=number()
+        print(a-b)
+
+    elif ch==3:
+        a,b=number()
+        print(a*b)
+
+    elif ch==4:
+        a,b=number()
+        print(a/b)
+
+    elif ch==5:
+        a,b=number()
+        print(a//b)
+
+    elif ch==6:
+        break
+    else:
+        print('Invalid choice')
+        '''
+#Types of argument
+#1 positional argument
+
+'''def sample(a,b):
+    print(a,b)
+sample(10,20)
+sample('asd',20)
+sample(['asd',20,21],20)  '''  
+
+#keyword argument
+
+'''def sample(name,age):
+    print('name:',name)
+    print('age:',age)
+sample('anu',20)
+sample(20,'arun')
+sample(age=20,name='akhil')'''
