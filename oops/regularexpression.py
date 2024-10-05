aa3ba8a83a5c20cf36fcea2f6829ad8a34a5986c
roshanrobin80 <roshanrobin220303@gmail.com>
2024-10-05 09:03:09 +0530

    regular expressions updated

--- oops/regularexpression.py
+++ oops/regularexpression.py
@@ -1,4 +1,4 @@
-import re
+'''import re
 a='welcome to jumanji'
 #print(re.sub('to','To',a))
 #print(re.sub('too','To',a))
@@ -39,4 +39,42 @@ a='abcd'
 #print(re.search('[a-z].*',a))
 #print(re.search('[a-z].+',a))
 #print(re.search('[a-z].?',a))
+#print(re.search('[a-z].*[0-9]',a))
+a='abcd9'
+#print(re.search('[a-z].*[0-9]',a))
+a='abcd789'
+#print(re.search('[a-z].*[0-9]',a))
+#print(re.search('[a-z].{3}',a))
+a='abcd'
+#print(re.search('[a-z].{3}',a))
+#print(re.search('[a-z].{4}',a))'''
+
+#programs
+
+#ph.no validation
+
+'''import re
+a='7306854116'
+if len(a)==10 and re.search('[6-9].{9}',a) and a.isdigit():
+    print('ph.no is valid')
+else:
+    print('no. not valid')'''
+
+#email validation
+
+import re
+'''a=str(input('enter email: '))
+if re.search('^[a-z].*@gmail.com$',a):
+    print('email is valid')
+else:
+    print('not valid')'''
+
+#password validation
+a=str(input('enter password: '))
+if re.search('^[A-z].*[@#$&.0-9]',a) and not(a.isdigit()) and len(a)>=8:
+    print('password is valid')
+else:
+    print('password not valid')
+
+
 
